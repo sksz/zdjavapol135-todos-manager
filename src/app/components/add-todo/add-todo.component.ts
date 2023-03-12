cc37b056b9f2a21565c56f0d7cfc5aab3d259283
Michał Szewczyk <michal.szewczyk@cyberfolks.pl>
2023-03-12 15:16:56 +0100

    Dodanie serwisu TodoService do komponentu AddTodoComponent i dodanie elementu Todo przez serwis

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -9,12 +10,16 @@ import { Todo } from 'src/app/models/Todo';
 export class AddTodoComponent {
   public title: string = '';
 
+  public constructor(
+    private todoService: TodoService
+  ) { }
+
   public addTodo(): void
   {
     let todo: Todo;
 
     todo = new Todo(this.title, false);
 
-    console.log(todo);
+    this.todoService.addTodoItem(todo);
   }
 }
